bdc43b92884572a38df72eef9322f859e6ab8de1
fcu-D0486437 <d0486437@fcu.edu.tw>
2016-12-07 23:52:48 +0800

    道館戰

--- GYM.java
+++ GYM.java
@@ -1,5 +1,43 @@
 package fcu.iecs.oop.pokemon;
 
+
 public class GYM {
+	public static Player fight(Player p1,Player p2)
+	{
+		int win1=0,win2=0;
+		
+		for(int i=0;i<3;i++)
+		{
+			if(p1.getPokemons()[i].getType()!=p2.getPokemons()[i].getType())
+			{
+				if(p1.getPokemons()[i].getType()==PokemonType.FIRE&&p2.getPokemons()[i].getType()==PokemonType.GRASS) win1++;
+				else if(p1.getPokemons()[i].getType()==PokemonType.GRASS&&p2.getPokemons()[i].getType()==PokemonType.WATER) win1++;
+				else if(p1.getPokemons()[i].getType()==PokemonType.WATER&&p2.getPokemons()[i].getType()==PokemonType.FIRE) win1++;
+				else win2++;
+			}
+			else if(p1.getPokemons()[i].getType()==p2.getPokemons()[i].getType())
+			{
+				if(p1.getPokemons()[i].getCp()>p2.getPokemons()[i].getCp()) win1++;
+				else if(p1.getPokemons()[i].getCp()==p2.getPokemons()[i].getCp())
+				{
+					int ran=(int)(Math.random()*2);
+					if(ran==0) win1++;
+					else win2++;
+				}
+				else win2++;
+			}
+		}
+		
+		if(win1>win2){
+			p1.setLevel(p1.getLevel()+1);
+			System.out.println("Winner is "+p1.getPlayerName()+", and his/her level becomes "+p1.getLevel()+".");
+			return p1;
+		}
+		else{
+			p2.setLevel(p2.getLevel()+1);
+			System.out.println("Winner is "+p2.getPlayerName()+", and his/her level becomes "+p2.getLevel()+".");
+			return p2;
+		}
+	}
 
 }
--- Player.java
+++ Player.java
@@ -1,5 +1,31 @@
 package fcu.iecs.oop.pokemon;
 
 public class Player {
+	private  String playerName;
+	private  Pokemon[] pokemons;
+	private  int level ;
+	public String getPlayerName() {
+		return playerName;
+	}
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+	}
+	public Pokemon[] getPokemons() {
+		return pokemons;
+	}
+	public void setPokemons(Pokemon[] pokemons) {
+		this.pokemons = pokemons;
+	}
+	public Player(String playerName) {
+		this.playerName = playerName;
+		this.level = 1;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
 
+	
 }
